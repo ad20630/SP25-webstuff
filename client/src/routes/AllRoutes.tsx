973e803026e76729d9caae9180aa8e589776f970
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-04-10 01:49:48 -0400

    changed the editor path to go through app route

--- client/src/routes/AllRoutes.tsx
+++ client/src/routes/AllRoutes.tsx
@@ -1,11 +1,7 @@
 import { RouteObject, Navigate } from "react-router-dom";
-import { HomeRoutes } from "./HomeRoutes/HomeRoutes";
-import { AppRoutes } from "./AppRoutes/AppRoutes";
 import ImageGallery from "components/Sidebar/ImageGallery";
-import Home from 'components/pages/Home';  // Assuming you have a Home page component
-import Editor from 'components/pages/app/editor/Editor';  // Assuming you have an Editor component
-//import Login from 'components/pages/Login';
-
+import Home from 'components/pages/Home';
+import AppPage from 'components/pages/app/AppPage';
 
 interface User {
   username: string;
@@ -13,22 +9,21 @@ interface User {
 }
 
 export const AllRoutes = (user: User | null, handleLogin: (username: string, token: string) => void, handleLogout: () => void): RouteObject[] => [
-  ...HomeRoutes,
-  ...AppRoutes,
   {
-    path: "/gallery",  // Add a new route for the image gallery
-    element: <ImageGallery onSelect={(url) => console.log("Selected image URL:", url)} />,
+    path: "/",
+    element: <Home />
   },
   {
-    path: '/',
-    element: <Home />,  // Public route
-  },/*
+    path: "/gallery",
+    element: <ImageGallery onSelect={(url) => console.log("Selected image URL:", url)} />
+  },
   {
-    path: '/login',
-    element: user ? <Navigate to="/" /> : <Login onLogin={handleLogin} />,  // If logged in, redirect to home
-  },*/
-  {
-    path: '/editor',
-    element: user ? <Editor /> : <Navigate to="/login" />,  // Protect this route, redirect if not logged in
+    path: "/app",
+    children: [
+      {
+        path: "editor",
+        element: user ? <AppPage /> : <Navigate to="/login" />
+      }
+    ]
   }
-]
+];
